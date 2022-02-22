0a6128335d3b06e5daf68db9de8e0af43d53bf39
Monika39-maker <dangolmonika@yahoo.com>
2022-02-22 12:59:01 +0000

    level 100 finished

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+const root = document.getElementById('root')
+const allEpisodeList = getAllEpisodes();
+let episodeNumber;
+// console.log(allEpisodeList)
+for (let i=0; i<allEpisodeList.length; i++) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const title = document.createElement('h3');
+
+  const seasonNumber = allEpisodeList[i].season;
+  episodeNumber = allEpisodeList[i].number
+  
+  
+  
+  if (episodeNumber < Number(10)) {
+    episodeNumber = '0' + allEpisodeList[i].number
+  } else {
+    episodeNumber = allEpisodeList[i].number;
+  }
+  console.log(episodeNumber)
+
+  title.innerText = `${allEpisodeList[i].name} - S0${seasonNumber}E${episodeNumber}`
+  
+  const image = document.createElement('img');
+  image.src = `${allEpisodeList[i].image['medium']}`
+  
+  
+  
+  const summary = document.createElement('div');
+  summary.innerHTML = `${(allEpisodeList[i].summary)}`
+  
+  card.appendChild(title);
+  card.appendChild(image)
+  
+  
+  card.appendChild(summary);
+  
+ 
+  root.appendChild(card)
+
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
-window.onload = setup;
+
+
